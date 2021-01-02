ccb71be984a1e03ac95904b0e6b429c6ccc3c508
Govind155 <73738015+Govind155@users.noreply.github.com>
2021-01-02 14:24:02 +0530

    Update init.py

--- init.py
+++ init.py
@@ -14,7 +14,7 @@ def predict():
     input_features = [float(x) for x in request.form.values()]
     value = np.array(input_features)
     
-     if input_features<0 or input_features>24:
+     if value<0 or value>24:
        return render_template('index.html', prediction_text='Please enter valid hours between 1 to 24 if you live on the Earth &#127757; .')
     
     output = model.predict([value])[0][0].round(2)
